d39acf112c10cbb9c39420e8e3a5f395646f11f4
Shaunak Basu <basushaunak@msn.com>
2021-09-30 18:20:59 +0530

    Done upto Ex 08 (Calculator)

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,71 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  let totalValue = 0;
+  total = +(num1 + num2);
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  let result = 0;
+  if(num1 > num2){
+    result = +(num1 - num2);
+  }	else {
+    result = +(num2 - num1);
+  }
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(arrayInput) {
+  let result = 0;
+  let arrayLen = arrayInput.length;
+  if (arrayLen === 0){
+    return 0;
+  }else if(arrayLen === 1){
+    return +(arrayInput[0]);
+  }
+  for(i = 0; i < arrayLen; i++){
+    result = result + arrayInput[i];
+  }
+  return +result;
 };
 
-const multiply = function() {
-
+const multiply = function(inputArray) {
+  let result = 1;
+  let arrayLen=inputArray.length;
+  if(arrayLen <= 1){
+    return NaN;
+  }
+  for(let i = 0; i < arrayLen; i++){
+    result = result * inputArray[i];
+  }
+  return +result;
 };
 
-const power = function() {
-	
+const power = function(num,power) {
+	if(num == 0){
+    return 0;
+  }
+  if(power == 0){
+    return 1;
+  }
+  let result = 1;
+  for(let i = 0; i < power; i++){
+    result = result * num;
+  }
+  return +result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num == 0 || num == 1){
+    return 1;
+  }
+  if(num < 0){
+    return NaN;
+  }
+  let result = 1;
+  for(i = num; i > 0; i--){
+    result = result * i;
+  }
+  return +result;
 };
 
 // Do not edit below this line
