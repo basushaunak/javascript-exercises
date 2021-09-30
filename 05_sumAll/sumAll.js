39c285c4433da36ce187fb6466e9c35a0473cd4a
Shaunak Basu <basushaunak@msn.com>
2021-09-30 12:43:37 +0530

    Done up to Ex 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(numFrom, numTo) {
+    let result = 0;
+    let numStart = numFrom;
+    let numEnd = numTo;
+    if(typeof numFrom !== "number"){
+        return "ERROR";
+    }
+    if(typeof numTo !== "number"){
+        return "ERROR";
+    }
+    if(numStart<0 || numEnd<0){
+        return "ERROR";
+    }
+    if(numFrom>numTo){
+        numStart = numTo;
+        numEnd = numFrom;
+    }
+    for(let i=numStart;i<=numEnd;i++){
+        result=result+i;
+    }
+    return result;
 };
 
 // Do not edit below this line
