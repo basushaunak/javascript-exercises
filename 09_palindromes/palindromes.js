b7c72fbb4c535fce805e4e00c0cf258600837ee9
Shaunak Basu <basushaunak@msn.com>
2021-09-30 20:13:42 +0530

    Done up to Exercise 09 (Palindrome)

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,40 @@
-const palindromes = function () {
-
+const palindromes = function (strInput) {
+    let revWord = [];
+    let word = [];
+    let char = "";
+    let asciiCode=0;
+    strInput = strInput.toLowerCase();
+    let lengthOfString = strInput.length;
+    if(lengthOfString==1){
+        return true;
+    }
+    if(lengthOfString==0){
+        return "ERROR";
+    }
+    for(let i = 0; i < lengthOfString; i++){
+        char = strInput[i];
+        asciiCode = char.charCodeAt();
+        if(asciiCode >= 48 && asciiCode <= 57){
+            word.push(char);
+            revWord.unshift(char);
+        } else if(asciiCode >= 97 && asciiCode <= 122){
+            word.push(char);
+            revWord.unshift(char);
+        }
+    }
+    lengthOfString = word.length;
+    let isPalindrome = true;
+    for(let i = 0; i < lengthOfString; i++){
+        if(word[i] !== revWord[i]){
+            isPalindrome = false;
+            break;
+        }
+    }
+    if (isPalindrome){
+        return true;
+    }else {
+        return false;
+    }
 };
 
 // Do not edit below this line
