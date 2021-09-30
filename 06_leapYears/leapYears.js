d1827b7f19389a997c053faca4f1ce4d61fc4a7a
Shaunak Basu <basushaunak@msn.com>
2021-09-30 12:53:04 +0530

    Done up to Ex 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(yearToCheck) {
+    if(typeof yearToCheck !== "number"){
+        return "ERROR";
+    }
+    if(yearToCheck%400 === 0){
+        return true;
+    }else if(yearToCheck%4===0 && yearToCheck%100 !== 0){
+        /* Number divided by 4 but not divided by 100 
+           Number divided by 100 must be divided by 400 to be a leap year */
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
