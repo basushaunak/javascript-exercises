4ad0da9d58194461be8374dc3ac45ff93d2a9cc3
Shaunak Basu <basushaunak@msn.com>
2021-10-01 05:35:45 +0530

    Done update Ex 10 (Fibonacci Sequence)

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    let rtnValue = 0;
+    let fiboArray = [];
+    position = parseInt(position);
+    if(position == 1 || position == 2){
+        return 1;
+    }
+    if(position < 0){
+        return "OOPS";
+    }
+    for (let i = 0; i < position; i++){
+        if(i == 0 || i == 1){
+            fiboArray.push(1);
+            continue;
+        }
+        rtnValue = fiboArray[i-1] + fiboArray[i-2];
+        fiboArray.push(rtnValue);
+    }
+    return rtnValue;
 };
 
 // Do not edit below this line
