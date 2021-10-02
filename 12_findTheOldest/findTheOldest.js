81b2db7524375a5bf35addb280a3de82a67ec0be
Shaunak Basu <basushaunak@msn.com>
2021-10-02 10:22:16 +0530

    Done upto Exercise 12 (findTheOldest)

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(objPeople) {
+    let currDate = new Date;
+    let currYear = currDate.getFullYear();
+    let oldest = -Infinity;
+    let oldestIndex = -1;
+    let age = -1;
+    let arrayLen = objPeople.length;
+    for(let i = 0; i < arrayLen ; i++){
+        if (typeof objPeople[i].yearOfDeath == "undefined" || objPeople[i].yearOfDeath == 0){
+            age = currYear - objPeople[i].yearOfBirth;
+        } else {
+            age = objPeople[i].yearOfDeath - objPeople[i].yearOfBirth;
+        }
+        if(age > oldest){
+            oldest = age;
+            oldestIndex = i;
+        }
+    }
+    return objPeople[oldestIndex];
 };
 
 // Do not edit below this line
